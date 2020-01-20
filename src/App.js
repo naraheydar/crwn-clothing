8d50794a6c4906ddb3ffae129fc46bf5ed1ecfb8
kheydarov84 <kheydarov84@gmail.com>
2020-01-20 12:33:04 -0400

    Added ssh key

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+
+const TopicsList = () => {
+	return(
+		<div>
+			<h1>TOPIC LIST PAGE</h1>
+		</div>
+	);
+}
+
+const TopicDetail = (props) => {
+	console.log(props)
+	return(
+		<div>
+			<h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+		</div>
+	);
+}
 
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
